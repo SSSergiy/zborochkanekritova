0fb4bc4b0c177596532bbc6003c7866ac4902571
SSSergiy <sergey1991serg@gmail.com>
2022-08-06 19:46:53 +0300

    first commit

---
+++ gulp/tasks/zip.js
@@ -0,0 +1,22 @@
+// import {deleteAsync} from 'del';
+// import del from "del";
+// import {deleteAsync} from "del"
+// export const reset = () => {
+//     return deleteAsync(['dist'])
+// }
+import zipPlugin from 'gulp-zip';
+export const zip = () => {
+  // del(`./${app.path.rootFolder}.zip`);
+  return app.gulp
+    .src(`${app.path.buildFolder}/**/*.*`, {})
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: 'ZIP',
+          message: 'Error: <%= error.message %>'
+        })
+      )
+    )
+    .pipe(zipPlugin(`${app.path.rootFolder}.zip`))
+    .pipe(app.gulp.dest('./'));
+};
